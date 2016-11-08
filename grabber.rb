4dea30215870623e3ec5e7ce56e4381d910224f0
Chris Heald <cheald@gmail.com>
2016-11-08 16:49:12 -0700

    Add shaman data, update grabber to skip some specs

--- grabber.rb
+++ grabber.rb
@@ -1,7 +1,8 @@
 require_relative './util'
+require 'net/http'
 
 KEY = ENV['API_KEY']
-TEMPLATE = "https://www.warcraftlogs.com/v1/rankings/encounter/%d?api_key=%s&class=8&spec=%d&bracket=%d&limit=1000&metric=dps&difficulty=%d"
+TEMPLATE = "https://www.warcraftlogs.com/v1/rankings/encounter/%d?api_key=%s&class=%d&spec=%d&bracket=%d&limit=1000&metric=dps&difficulty=%d"
 
 desired_class = ARGV[0]
 slug = class_slug(desired_class)
@@ -11,22 +12,25 @@ BRACKETS = (6..16)
 DIFFICULTY_NAMES = %w(Normal Heroic Mythic)
 
 raise "Could not find specs for class: #{desired_class}" if KLASS.nil?
+class_id = KLASS["id"]
 
-bar = ProgressBar.new(KLASS["specs"].length * DIFFICULTIES.length * BRACKETS.to_a.length)
+encounters = get_zones.detect {|z| z["id"] == 10 }["encounters"]
+bar = ProgressBar.new(encounters.length * KLASS["specs"].length * DIFFICULTIES.length * BRACKETS.to_a.length)
 
 target_dir = File.join("data", slug)
 Dir.mkdir(target_dir) unless Dir.exists?(target_dir)
 
-get_zones.detect {|z| z["id"] == 10 }["encounters"].each do |encounter|
+SKIP_SPECS = %w(Restoration Combat Holy Protection Guardian Vengence Blood)
+encounters.each do |encounter|
   KLASS["specs"].each do |spec|
-    next if spec["name"] == "Combat"
+    next if SKIP_SPECS.include?(spec["name"])
     DIFFICULTIES.each do |difficulty_id, difficulty_name|
       BRACKETS.each do |bracket|
         bar.increment!
-        url = format(TEMPLATE, encounter["id"], KEY, spec["id"], bracket, difficulty_id)
+        url = format(TEMPLATE, encounter["id"], KEY, class_id, spec["id"], bracket, difficulty_id)
         file = File.join target_dir, format("%s-%s-%s-%s.json", encounter["name"].gsub(/[^a-z]/i, "_"), spec["name"], bracket, difficulty_name)
         unless File.exists?(file)
-          json = open(url).read
+          json = Net::HTTP.get(URI.parse(url))
           open(file, "w") {|fp| fp.puts json }
         end
       end
